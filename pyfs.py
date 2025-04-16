9fe375feccd1cd2fdfaea19ea8414108e5675d59
thetrueduck420 <thetrueduck420@gmail.com>
2025-04-16 13:14:55 +0300

    test

---
+++ pyfs.py
@@ -0,0 +1,37 @@
+import numpy as np
+fs = [];
+
+
+contents = 0;
+i = 0;
+
+def addFile(filename, filecontents):
+    global contents;
+
+    fs.insert(contents, filename);
+    contents = contents + 1;
+    fs.insert(contents, filecontents);
+    contents = contents + 1;
+
+def readFile(filename):
+    global i;
+    while (fs[i] != filename):
+        i = i + 1;
+
+    i = i + 1;
+    return fs[i];
+    i = 0;
+
+def delFile(filename):
+    global i;
+    while (fs[i] != filename):
+        i = i + 1;
+    fs[i] = NULL;
+    fs[i + 1] = NULL;
+    i = 0;
+    
+
+addFile("test", "test1");
+print(readFs("test"));
+delFile("test");
+
